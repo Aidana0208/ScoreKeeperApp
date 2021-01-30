60f9a0a924bf426634d30d49dcac183e6a375f0d
aidana <aydana.moldokanovna@gmail.com>
2021-01-30 18:15:03 +0600

    Initial commit

---
+++ app/src/main/java/com/example/scorekeeperapp/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.scorekeeperapp;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This activity keeps track of the basketball score for 2 teams.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // Tracks the score for Team A
+    int scoreTeamA = 0;
+
+    // Tracks the score for Team B
+    int scoreTeamB = 0;
+
+    int foulTeamA = 0;
+
+    int foulTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneFoulTeamA(View v) {
+        foulTeamA = foulTeamA + 1;
+        displayFoulTeamA(foulTeamA);
+    }
+
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneFoulTeamB(View v) {
+        foulTeamB = foulTeamB + 1;
+        displayFoulTeamB(foulTeamB);
+    }
+
+    /**
+     * Resets the score for both teams back to 0.
+     */
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+
+        foulTeamA = 0;
+        foulTeamB = 0;
+        displayFoulTeamA(foulTeamA);
+        displayFoulTeamB(foulTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayFoulTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_foul);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayFoulTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_foul);
+        scoreView.setText(String.valueOf(score));
+    }
+}
